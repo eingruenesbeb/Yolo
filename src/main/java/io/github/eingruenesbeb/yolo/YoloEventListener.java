d0fb0c831284064273413bd074abcd74fe3d4baa
Tim R <agreenbeb@gmail.com>
2023-03-05 22:46:21 +0100

    Added license

--- src/main/java/io/github/eingruenesbeb/yolo/YoloEventListener.java
+++ src/main/java/io/github/eingruenesbeb/yolo/YoloEventListener.java
@@ -1,3 +1,22 @@
+/*
+ * This is program is a plugin for Minecraft Servers called "Yolo".
+ * Copyright (C) 2023  eingruenesbeb
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can reach the original autor via e-Mail: agreenbeb@gmail.com
+ */
+
 package io.github.eingruenesbeb.yolo;
 
 import me.leoko.advancedban.bukkit.BukkitMethods;
---
+++ src/main/java/io/github/eingruenesbeb/yolo/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package is the main package for the minecraft server plugin "Yolo".
+ * It is licensed under the <a href="https://spdx.org/licenses/GPL-3.0-or-later.html">GPL-3.0-or-later</a> license.
+ * @version 0.3.0
+ */
+package io.github.eingruenesbeb.yolo;
