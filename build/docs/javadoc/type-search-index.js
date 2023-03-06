0f719fbec205586b7b4088db1e975fbb27a7bca6
eingruenesbeb <agreenbeb@gmail.com>
2023-03-06 02:14:29 +0100

    Delete build directory
    Unnecessary, because the java-doc is now generated in "./docs".
    This directory can be ignored in the future.

--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.eingruenesbeb.yolo","l":"Yolo"},{"p":"io.github.eingruenesbeb.yolo","l":"YoloEventListener"},{"p":"io.github.eingruenesbeb.yolo","l":"YoloPluginCommandSender"}];updateSearchResults();
