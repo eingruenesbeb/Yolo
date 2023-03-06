53003d01ccfc82c15fb0695d2f352d0fb6b03d1a
Tim R <agreenbeb@gmail.com>
2023-03-06 04:18:33 +0100

    style: Fixed grammar error in copyright notice.
    [skip ci]

--- src/main/java/io/github/eingruenesbeb/yolo/SpicordManager.java
+++ src/main/java/io/github/eingruenesbeb/yolo/SpicordManager.java
@@ -14,7 +14,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * You can reach the original autor via e-Mail: agreenbeb@gmail.com
+ * You can reach the original author via e-Mail: agreenbeb@gmail.com
  */
 
 package io.github.eingruenesbeb.yolo;
