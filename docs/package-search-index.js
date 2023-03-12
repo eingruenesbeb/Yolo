5dba3f7ee6daf4b8ef78752591a5e49b46283786
Tim R <agreenbeb@gmail.com>
2023-03-12 19:10:40 +0100

    docs: Updated documentation.

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.eingruenesbeb.yolo"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.eingruenesbeb.yolo"},{"l":"io.github.eingruenesbeb.yolo.managers"}];updateSearchResults();
