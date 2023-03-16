a470b340b07a9e114e0bdd885aa0b14dfe6b7403
Tim R <agreenbeb@gmail.com>
2023-03-16 21:45:29 +0100

    feat: Began work on commands (to be amended)
    [skip ci]

---
+++ src/main/java/io/github/eingruenesbeb/yolo/commands/CommandRegistrar.java
@@ -0,0 +1,64 @@
+/*
+ * This is program is a plugin for Minecraft Servers called "Yolo".
+ * Copyright (C) 2023  eingruenesbeb
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can reach the original author via e-Mail: agreenbeb@gmail.com
+ */
+
+package io.github.eingruenesbeb.yolo.commands;
+
+import io.github.eingruenesbeb.yolo.Yolo;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+
+public class CommandRegistrar {
+    private enum Commands {
+        RELOAD,
+        REVIVE;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+
+        @Nullable CommandExecutor getCommandInstance() {
+            switch (this) {
+                case RELOAD -> {
+                    return new ReloadCommand();
+                }
+                case REVIVE -> {
+                    return new ReviveCommand();
+                }
+            }
+            return null;
+        }
+    }
+
+    public void registerCommands() {
+        for (Commands command : Commands.values()) {
+            PluginCommand pluginCommand = Yolo.getPlugin(Yolo.class).getCommand(command.toString());
+            if (pluginCommand == null) continue;
+            pluginCommand.setExecutor(command.getCommandInstance());
+
+            if (Arrays.stream(command.getClass().getInterfaces()).anyMatch(i -> i == TabExecutor.class)) {
+                pluginCommand.setTabCompleter((TabExecutor) command.getCommandInstance());
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/eingruenesbeb/yolo/commands/ReviveCommand.java
@@ -0,0 +1,40 @@
+/*
+ * This is program is a plugin for Minecraft Servers called "Yolo".
+ * Copyright (C) 2023  eingruenesbeb
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can reach the original author via e-Mail: agreenbeb@gmail.com
+ */
+
+package io.github.eingruenesbeb.yolo.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ReviveCommand implements TabExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
